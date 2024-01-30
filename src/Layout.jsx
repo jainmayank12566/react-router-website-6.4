4688878e5bdade155df8f449379b94641338c662
jainmayank12566 <jainmayank12566@gmail.com>
2024-01-30 16:08:32 +0530

    firstcommit

---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+function Layout() {
+  return (
+    <div>
+    <Header/>
+    <Outlet />
+    <Footer />
+    </div>
+  )
+}
+
+export default Layout;
---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
+import Contact from './components/Contact.jsx'
+import User from './components/User.jsx'
+import Github, { sayhello } from './components/Github.jsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={sayhello}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router}>
+    <App/>
+  </RouterProvider>
+)
